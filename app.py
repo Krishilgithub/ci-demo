64b5d8760c049dd32fefa7081c8640ba21ca9ba6
Krishilgithub <100427701+Krishilgithub@users.noreply.github.com>
2025-06-12 19:49:08 +0530

    initail commit

---
+++ app.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a+b
+
+
+
+
+def subtract(a, b):return a-b
+
+result1 = add(5, 3)
+result2 = subtract(10, 4)
+
+print("Addition Result:", result1)
+print("Subtraction Result:", result2)
